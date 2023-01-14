d54c8773d192f8943f3ba366879bb87fd46e8ffd
Vivian Li <viviannnlyr23@gmail.com>
2023-01-14 18:52:38 -0500

    hold dice functionality done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Dice from "./components/Dice";
 import "./style.css"
+import { nanoid } from 'nanoid'
 
 export default function App() {
     function allNewDice() {
         const diceArray = []
         for (let i = 0; i < 10; i++) {
-            diceArray.push(Math.floor(Math.random() * 6 + 1))
+            diceArray.push({
+                value: Math.floor(Math.random() * 6 + 1),
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return diceArray
     }
@@ -14,14 +19,36 @@ export default function App() {
     const [dice, setDice] = React.useState(allNewDice())
     
     function rollDice() {
-        setDice(allNewDice())
+        setDice(prevDice => prevDice.map(item => {
+            return item.isHeld ? item : {
+                value: Math.floor(Math.random() * 6 + 1),
+                isHeld: false,
+                id: nanoid()
+            }
+        }))
     }
     
-   const diceElements = dice.map(item => <Dice value={item} />)
+    function holdDice(id) {
+        setDice(prevDice => {
+            return prevDice.map(item => {
+                return item.id === id ? {
+                    ...item, isHeld: !item.isHeld} : item
+            })
+        })
+    }
+    const diceElements = dice.map(item => 
+    <Dice 
+        value={item.value} 
+        isHeld={item.isHeld} 
+        key={item.id}
+        handleClick={() => holdDice(item.id)} 
+        />)
     
     return (
         <main>
-            <div className="dice-container">
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all the dice are the same. Click each dice to freeze it at its current value between rolls.</p>
+            <div className="dice-container" >
                 {diceElements}
             </div>
             <button type="button" className="button" onClick={rollDice}>Roll</button>
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 export default function Dice(props) {
+    const style = {
+        backgroundColor: props.isHeld? "#59E391": "white",
+    }
+
     return (
-        <div className="die">
+        <div 
+            className="die" 
+            style={style} 
+            onClick={props.handleClick}
+        >
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
